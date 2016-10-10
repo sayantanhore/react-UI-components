ac452be45cde905665bd156a98d4611612183d1a
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-10-11 02:10:02 +0300

    webpack.config.js updated ti correct Loader config error, string quote removed

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
   },
   module: {
     loaders: [{
-      test: '/.js$',
-      exclude: '/node_modules/',
+      test: /.js$/,
+      exclude: /node_modules/,
       loader: 'babel',
       query: {
         presets: ['es2015', 'react']
